05a13362eaef491e62d9aacc8ad7dd653588ed36
Risumi <rizkysuhaimi@gmail.com>
2018-12-03 23:16:27 +0700

    My Django app, first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+a=2
+b=3
+c=a+b
+print(c)
+if(a>b):
+    print('asu')
+else:
+    print('kontlo'+str(a))
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    print (name)
+
+
