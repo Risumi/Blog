fe40487827b8b34885485e6a6388e72868b5ad34
Risumi <rizkysuhaimi@gmail.com>
2018-12-31 00:16:04 +0700

    Reworked style Added css login

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from .models import Post
+from .forms import PostForm
 from django.utils import timezone
 from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +14,46 @@ def post_list(request):
 def post_detail(request,pk):
     posts = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html',{'post':posts})
+
+@login_required
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=post.pk)
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
